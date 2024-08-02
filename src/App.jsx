c8014bbc8f6296b23b4c317f75b1e6fd6b69c7b6
WalSeq <waltereseq@gmail.com>
2024-08-02 20:56:52 -0300

    added profile picture

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,27 @@ import Footer from './components/footer'
 import Header from './components/header'
 import Cards from './components/cards'
 import './App.css'
+import profilePicture from './assets/20240724_092946.jpg'
 
 function App() {
 
 
   return (
     <>
-    <Header></Header>
+      <Header></Header>
       <div>
+        <img className='profile-img' src={profilePicture} alt="Foto de perfil" />
+        <div>
+          <h1>Curriculum Vitae</h1>
+          <h1>Sequeira Walter Emanuel</h1>
+        </div>
       </div>
-      <h1>Curriculum Vitae</h1>
-      <h1>Sequeira Walter Emanuel</h1>
       <a href="#header">
-        <Cards/>
-      <p className="read-the-docs">
-        Clic para volver a las secciones
-      </p></a>
-      <Footer/>
+        <Cards />
+        <p className="read-the-docs">
+          Clic para volver a las secciones
+        </p></a>
+      <Footer />
     </>
   )
 }
